40e8358173a71a18ef574ced9c801bde2f734fc5
John Gibbs <johngibbs110@gmail.com>
2022-03-25 16:12:12 -0500

    Solution to Day 1, Part 1, 2020

---
+++ 2020/Day 1/script.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var input = fs.readFileSync('input.txt').toString().split("\n").map(Number);
+
+// Solution for Part One
+
+let result = 0
+
+for (let i = 0; i < input.length; i++) {
+
+    numberToTry = input[i];
+
+    for (let j = 1; j < input.length; j++) {
+
+        if (numberToTry + input[j] == 2020) {
+
+            result = numberToTry * input[j];
+
+        }
+
+    }
+
+}
+
+console.log(result);
