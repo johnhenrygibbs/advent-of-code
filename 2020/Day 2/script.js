57bc4d243f52f3db2ba01e1fa579e0b30bd325ad
John Gibbs <hello@johnhenrygibbs.com>
2022-03-29 21:35:44 -0500

    Solution for Part One

--- 2020/Day 2/script.js
+++ 2020/Day 2/script.js
@@ -1,4 +1,45 @@
 var fs = require('fs');
 var input = fs.readFileSync('2020/Day 2/input.txt').toString().split("\n");
 
-console.log(input);
+// Solution for Part One
+
+let array = [];
+let policy = "";
+let policyAsArray = [];
+let letter = "";
+let password = "";
+
+let totalLetters = 0;
+let validPasswords = 0; 
+
+for (let i = 0; i < input.length; i++) {
+
+    array = input[i].split(" ");
+
+    policy = array[0];
+    letter = array[1];
+    password = array[2];
+
+    policyAsArray = policy.toString().split("-").map(Number);
+
+    for (let j = 0; j < password.length; j++) {
+
+        if (letter[0] == password[j]) {
+
+            totalLetters++;
+
+        }
+
+    }
+
+    if (totalLetters >= policyAsArray[0] && totalLetters <= policyAsArray[1]) {
+
+        validPasswords++;
+
+    }
+
+    totalLetters = 0; 
+
+}
+
+console.log(validPasswords);
