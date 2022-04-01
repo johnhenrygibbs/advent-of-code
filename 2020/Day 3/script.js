f2a2dc39f3ccc63c0374cf8d7d77c039f18d01fb
John Gibbs <hello@johnhenrygibbs.com>
2022-03-31 19:34:42 -0500

    Solution to Part One

--- 2020/Day 3/script.js
+++ 2020/Day 3/script.js
@@ -1,6 +1,42 @@
 var fs = require('fs');
 var input = fs.readFileSync("2020/Day 3/input.txt").toString().split("\n");
 
-console.log(input);
-
 // Solution for Part One 
+
+let totalTrees = 0; 
+let spaceToCheck = ''; 
+let position = 0; 
+
+for (let i = 1; i < input.length; i++) {
+
+    position += 3;
+
+    if (position == 31) {
+
+        position = 0;
+
+    }
+    
+    if (position == 32) {
+
+        position = 1;
+
+    } 
+    
+    if (position == 33) {
+
+        position = 2;
+
+    }
+
+    spaceToCheck = input[i][position];
+
+    if (spaceToCheck == '#') {
+
+        totalTrees++;
+
+    }
+
+}
+
+console.log(totalTrees);
