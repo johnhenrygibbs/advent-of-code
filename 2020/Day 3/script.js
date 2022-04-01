bac2fcc34fbfffcd81a828e14d22c008289acc45
John Gibbs <hello@johnhenrygibbs.com>
2022-03-31 22:00:25 -0500

    Solution to Part Two

--- 2020/Day 3/script.js
+++ 2020/Day 3/script.js
@@ -1,42 +1,132 @@
 var fs = require('fs');
 var input = fs.readFileSync("2020/Day 3/input.txt").toString().split("\n");
 
-// Solution for Part One 
+// Solution for Part One (checking the first slope, numbered to reflect part two ordering)
 
-let totalTrees = 0; 
+let totalOne = 0;
+let totalTwo = 0;
+let totalThree = 0;
+let totalFour = 0;
+let totalFive = 0; 
+
+let positionOne = 0; 
+let positionTwo = 0; 
+let positionThree = 0; 
+let positionFour = 0; 
+let positionFive = 0;
+
 let spaceToCheck = ''; 
-let position = 0; 
 
 for (let i = 1; i < input.length; i++) {
 
-    position += 3;
+    positionTwo += 3;
 
-    if (position == 31) {
+    if (positionTwo >= 31) {
 
-        position = 0;
-
-    }
-    
-    if (position == 32) {
-
-        position = 1;
-
-    } 
-    
-    if (position == 33) {
-
-        position = 2;
+        positionTwo = positionTwo - 31;
 
     }
 
-    spaceToCheck = input[i][position];
+    spaceToCheck = input[i][positionTwo];
 
     if (spaceToCheck == '#') {
 
-        totalTrees++;
+        totalTwo++;
 
     }
 
 }
 
-console.log(totalTrees);
+console.log(totalTwo);
+
+// Solution for Part Two (checking the other four slopes)
+
+for (let i = 1; i < input.length; i++) {
+
+    positionOne += 1;
+
+    if (positionOne >= 31) {
+
+        positionOne = positionOne - 31;
+
+    }
+
+    spaceToCheck = input[i][positionOne];
+
+    if (spaceToCheck == '#') {
+
+        totalOne++;
+
+    }
+
+}
+
+console.log(totalOne);
+
+for (let i = 1; i < input.length; i++) {
+
+    positionThree += 5;
+
+    if (positionThree >= 31) {
+
+        positionThree = positionThree - 31;
+
+    }
+
+    spaceToCheck = input[i][positionThree];
+
+    if (spaceToCheck == '#') {
+
+        totalThree++;
+
+    }
+
+}
+
+console.log(totalThree);
+
+for (let i = 1; i < input.length; i++) {
+
+    positionFour += 7;
+
+    if (positionFour >= 31) {
+
+        positionFour = positionFour - 31;
+
+    }
+
+    spaceToCheck = input[i][positionFour];
+
+    if (spaceToCheck == '#') {
+
+        totalFour++;
+
+    }
+
+}
+
+console.log(totalFour);
+
+for (let i = 2; i < input.length; i += 2) {
+
+    positionFive += 1;
+
+    if (positionFive >= 31) {
+
+        positionFive = positionFive - 31;
+
+    }
+
+    spaceToCheck = input[i][positionFive];
+
+    if (spaceToCheck == '#') {
+
+        totalFive++;
+
+    }
+
+}
+
+console.log(totalFive);
+
+console.log(totalOne * totalTwo * totalThree * totalFour * totalFive);
