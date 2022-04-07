59a78dfb47c2894ff465c445b1e21066b4e1ac74
John Gibbs <hello@johnhenrygibbs.com>
2022-04-07 15:36:22 -0500

    Solution for Part One

---
+++ 2020/Day 7/script.js
@@ -0,0 +1,65 @@
+var fs = require('fs');
+var input = fs.readFileSync("2020/Day 7/input.txt").toString().split("\n");
+
+// Solution for Part One
+
+let validBagColors = [];
+
+// Recursive function that returns how many bag colors can hold at least one shiny gold bag.
+
+function getValidBags(input) { 
+
+    let matches = 0;
+    let rule = "";
+
+    // First loop gathers up the primary bags (one level up) that directly hold shiny gold bags and is ignored each time the function recurs.
+
+    for (let i = 0; i < input.length; i++) {
+
+        rule = input[i].split(" bags contain ");
+
+        if (rule[1].includes("shiny gold")) {
+
+            validBagColors.unshift(rule[0]);
+
+            input.splice(i, 1);
+            
+        }
+
+    }
+
+    // Second loop is nested and iterates through the input file and the array of valid bags looking for deeper matches with secondary bags (more than one level up).
+
+    for (let i = 0; i < input.length; i++) {
+
+        for (let j = 0; j < validBagColors.length; j++) {
+
+            rule = input[i].split(" bags contain "); 
+
+            if (rule[1].includes(validBagColors[j])) {
+
+                matches++;
+
+                validBagColors.unshift(rule[0]); 
+
+                input.splice(i, 1);
+
+            }
+
+        }
+
+    }
+
+    // Condition that either reruns the function until no matches are found or returns the length of the valid bag array.
+
+    while (matches != 0) {
+
+        return getValidBags(input);
+
+    } 
+
+    return validBagColors.length;
+
+}
+
+console.log(getValidBags(input));
