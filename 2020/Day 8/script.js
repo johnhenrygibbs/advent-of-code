8f82cbc8aebe326d0f2598f5e54509ae62f567b2
John Gibbs <hello@johnhenrygibbs.com>
2022-04-13 17:29:42 -0500

    Solution for Part One

--- 2020/Day 8/script.js
+++ 2020/Day 8/script.js
@@ -1,4 +1,49 @@
 var fs = require('fs');
 var input = fs.readFileSync("2020/Day 8/input.txt").toString().split("\n");
 
-console.log(input);
+// Solution for Part One
+
+let completedInstructions = [];
+let instructions = [];
+let accumulator = 0;
+let position = 0;
+
+function bootCode() {
+
+    instructions = input[position].split(" ");
+
+    // A series of conditionals that check whether the action is an accumulator, jump, or no operation and updates the accumlator and position variables accordingly.
+
+    if (instructions[0] == "acc") { 
+
+        accumulator += Number(instructions[1]);
+
+        position++;
+
+    } else if (instructions[0] == "nop") {
+
+        position++;
+
+    } else if (instructions[0] == "jmp") {
+
+        position += Number(instructions[1]);
+
+    }
+
+    // Conditional that pushes completed instructions to an array if no duplicate positions are found. If a duplicate position is found, then the accumulator variable is returned.
+
+    if (!completedInstructions.includes(position)) {
+
+        completedInstructions.unshift(position);
+
+        return bootCode();
+
+    } else {
+
+        return accumulator;
+
+    }
+
+}
+
+console.log(bootCode());
