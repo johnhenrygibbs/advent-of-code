a4efdb04b139bf87479a28edf4222bd506ee9e85
John Gibbs <hello@johnhenrygibbs.com>
2022-05-12 14:32:58 -0500

    Solution for Part Two

--- 2020/Day 13/script.js
+++ 2020/Day 13/script.js
@@ -12,27 +12,27 @@ let earliestDepartureTimes = [];
 
 for (let i = 0; i < busesList.length; i++) {
 
-    if (busesList[i] != 'x') {
+	if (busesList[i] != 'x') {
 
-        busesInService.push(busesList[i]);
+		busesInService.push(busesList[i]);
 
-    }
-    
+	}
+
 }
 
 // Helper function that determines the closest departure time of a given bus to my departure time.
 
 function departureLoop(number) {
 
-    let closestDepartureTime = 0;
+	let closestDepartureTime = 0;
 
-    while (closestDepartureTime < myDepartureTime) {
+	while (closestDepartureTime < myDepartureTime) {
 
-        closestDepartureTime += number;
+		closestDepartureTime += number;
 
-    }
+	}
 
-    return closestDepartureTime;
+	return closestDepartureTime;
 
 }
 
@@ -40,17 +40,17 @@ function departureLoop(number) {
 
 function calculateDepartureTimes() {
 
-    for (let i = 0; i < busesInService.length; i++) {
+	for (let i = 0; i < busesInService.length; i++) {
 
-        let busToCheck = Number(busesInService[i]);
+		let busToCheck = Number(busesInService[i]);
 
-        let earliestDepartureTime = departureLoop(busToCheck);
+		let earliestDepartureTime = departureLoop(busToCheck);
 
-        earliestDepartureTimes.push(earliestDepartureTime);
+		earliestDepartureTimes.push(earliestDepartureTime);
 
-    }
+	}
 
-    return earliestDepartureTimes;
+	return earliestDepartureTimes;
 
 }
 
@@ -58,31 +58,31 @@ function calculateDepartureTimes() {
 
 function findEarliestBus() {
 
-    let closestBusTimes = calculateDepartureTimes();
+	let closestBusTimes = calculateDepartureTimes();
 
-    let differencesArray = [];
+	let differencesArray = [];
 
-    for (let i = 0; i < closestBusTimes.length; i++) {
+	for (let i = 0; i < closestBusTimes.length; i++) {
 
-        differencesArray.push(closestBusTimes[i] - myDepartureTime);
+		differencesArray.push(closestBusTimes[i] - myDepartureTime);
 
-    }
+	}
 
-    let lowest = differencesArray[0];
-    let index = 0; 
+	let lowest = differencesArray[0];
+	let index = 0;
 
-    for (let j = 1; j < differencesArray.length; j++) {
+	for (let j = 1; j < differencesArray.length; j++) {
 
-        if (differencesArray[j] < lowest) {
+		if (differencesArray[j] < lowest) {
 
-            lowest = differencesArray[j];
-            index = differencesArray.indexOf(lowest);
-            
-        }
+			lowest = differencesArray[j];
+			index = differencesArray.indexOf(lowest);
 
-    }
+		}
 
-    return lowest * busesInService[index];
+	}
+
+	return lowest * busesInService[index];
 
 }
 
@@ -90,3 +90,56 @@ console.log(findEarliestBus());
 
 // Solution for Part Two
 
+// Function that finds how many minutes after each subsequent bus arrives at the station by grabbing the index of each number in the input data.
+
+function findMinutesAfter(input) {
+
+	let minutes = [];
+
+	for (let i = 0; i < input.length; i++) {
+
+		if (input[i] != 'x') {
+
+			minutes.push(input.indexOf(input[i]));
+
+		}
+
+	}
+
+	return minutes;
+
+}
+
+let minutesAfter = findMinutesAfter(busesList);
+
+function findEarliestTimestamp() {
+
+	let t = 0;
+	let timestamp;
+	let interval = Number(busesInService[0]);
+
+	// Loop that iterates through the buses in service and looks for match between first two buses.
+
+	for (let i = 0; i < busesInService.length - 1; i++) {
+
+		// While no matches are found, we increase 't' by the present interval and keep looking.
+
+		while ((t + minutesAfter[i]) % Number(busesInService[i]) != (t + minutesAfter[i + 1]) % Number(busesInService[i + 1])) {
+
+			t += interval;
+
+		}
+
+		// Once a match is found 't' is saved as the timestamp and the interval is increased to match the subsequent point where the departure times for buses one and two already match, thus allowing the function to iterate through the loop faster.
+
+		timestamp = t;
+
+		interval = interval * Number(busesInService[i + 1]);
+
+	}
+
+	return timestamp;
+
+}
+
+console.log(findEarliestTimestamp());
