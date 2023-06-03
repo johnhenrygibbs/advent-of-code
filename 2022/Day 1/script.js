f36d9e818c4942d73992b5d35e6bfb5dba92bc4d
John Gibbs <hello@johnhenrygibbs.com>
2023-06-03 11:44:24 -0700

    Solution for Part Two

--- 2022/Day 1/script.js
+++ 2022/Day 1/script.js
@@ -25,3 +25,33 @@ for (let i = 0; i < input.length; i++) {
 }
 
 console.log(biggest);
+
+// Solution for Part Two
+
+// Create an elves array to hold the total calories for each elf as we iterate through the input data.
+
+let elves = [];
+let calories = 0;
+
+for (let i = 0; i < input.length; i++) {
+
+    if (input[i] === 0) {
+
+        elves.push(calories);
+        calories = 0;
+
+    }
+
+    calories += input[i];
+
+}
+
+// Sort the elves array in descending order and sum the top three elves.
+
+elves.sort(function(a, b) {
+
+    return b - a;
+
+});
+
+console.log(elves[0] + elves[1] + elves[2]);
