894573adec2cf0cc08f19c56b872713907e3663a
John Gibbs <hello@johnhenrygibbs.com>
2023-06-07 17:05:46 -0700

    Solution for Part One

---
+++ 2022/Day 4/script.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var input = fs.readFileSync('2022/Day 4/input.txt').toString().split("\n");
+
+// Solution for Part One
+
+let validPairs = 0;
+
+for (let i = 0; i < input.length; i++) {
+
+    let pairs = input[i].split(",");
+    let firstElf = pairs[0].split("-");
+    let secondElf = pairs[1].split("-");
+
+    // Once we split each pair into an array, convert the strings to numbers.
+
+    for (let i = 0; i < firstElf.length; i++) {
+
+        firstElf[i] = Number(firstElf[i]);
+        secondElf[i] = Number(secondElf[i]);
+
+    }
+
+    // Uses helper function to test whether the ranges overlap. If so, validPairs is incremented.
+
+    let testOne = containsRange(firstElf, secondElf);
+    let testTwo = containsRange(secondElf, firstElf);
+
+    if (testOne === true || testTwo === true) {
+
+        validPairs++;
+
+    }
+
+}
+
+console.log(validPairs);
+
+// Helper function to check whether a paired number range is contained within another.
+
+function containsRange(first, second) {
+
+    if (first[0] >= second[0] && first[1] <= second[1]) {
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}
