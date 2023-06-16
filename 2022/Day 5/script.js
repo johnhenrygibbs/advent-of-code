67718db3551e5bcb84a52593002a1736a80d9e28
John Gibbs <hello@johnhenrygibbs.com>
2023-06-15 17:52:31 -0700

    Solution for Part Two

--- 2022/Day 5/script.js
+++ 2022/Day 5/script.js
@@ -15,6 +15,22 @@ let stacks = {
 
 }
 
+// Creating stacksClone for Part Two.
+
+let stacksClone = {
+
+    1: ['H', 'T', 'Z', 'D'],
+    2: ['Q', 'R', 'W', 'T', 'G', 'C', 'S'],
+    3: ['P', 'B', 'F', 'Q', 'N', 'R','C', 'H'],
+    4: ['L', 'C', 'N', 'F', 'H', 'Z'],
+    5: ['G', 'L', 'F', 'Q', 'S'],
+    6: ['V', 'P', 'W', 'Z', 'B', 'R', 'C', 'S'],
+    7: ['Z', 'F', 'J'],
+    8: ['D', 'L', 'V', 'Z', 'R', 'H', 'Q'],
+    9: ['B', 'H', 'G', 'N', 'F', 'Z', 'L', 'D']
+
+}
+
 // Solution for Part One
 
 for (let i = 0; i < input.length; i++) {
@@ -22,20 +38,36 @@ for (let i = 0; i < input.length; i++) {
     let step = input[i];
     let commands = isolateNumbers(step);
     let moves = commands[0];
+    let cratesToMove = [];
 
     while (moves > 0) {
 
         let crate = stacks[commands[1]].pop();
+        let crateClone = stacksClone[commands[1]].pop();
         stacks[commands[2]].push(crate);
+        cratesToMove.unshift(crateClone);
         moves--;
 
     }
 
+    // Solution for Part Two
+
+    // After moving crate clones into a separate array to preserve order, iterate over the array to update stacksClone and reset array.
+
+    for (let j = 0; j < cratesToMove.length; j++) {
+
+        stacksClone[commands[2]].push(cratesToMove[j]);
+
+    }
+
+    cratesToMove = [];
+
 }
 
-let result = "";
-
 // Iterating through the new order of stacks, concatenate each array's final letter for the end result.
+
+let result = "";
+let resultClone = "";
 
 for (let stack in stacks) {
 
@@ -43,7 +75,14 @@ for (let stack in stacks) {
 
 }
 
+for (let stack in stacksClone) {
+
+    resultClone += stacksClone[stack].pop();
+
+}
+
 console.log(result);
+console.log(resultClone);
 
 // Helper function isolates the numbers found within a given string and returns them in an ordered array.
 
