20310093546bba37c9338f5e41721cfe8bc161f7
John Gibbs <hello@johnhenrygibbs.com>
2023-12-06 15:34:42 -0800

    Solution for Part Two

--- 2023/Day 2/script.py
+++ 2023/Day 2/script.py
@@ -45,3 +45,48 @@ def cubeGame(input):
     return sum
 
 print(cubeGame(input))
+
+# Solution for Part Two
+
+def cubed(input):
+
+    sum = 0
+
+    # Same iterative setup as in part one.
+
+    for line in input:
+
+        dict = {}
+        data = line.split(":")
+        games = data[1].strip().replace(";", ",").split(", ")
+
+        for outcome in games:
+
+            handful = outcome.split(" ")
+            handful[0] = int(handful[0])
+
+            # If the color hasn't appeared yet, update the dict with the color and number.
+
+            if handful[1] not in dict:
+
+                dict.update({handful[1] : int(handful[0])})
+
+            # If the color has appeared, check to see if the present number is greater than its previous largest appearance and update accordingly.
+
+            elif handful[1] in dict:
+
+                minimum = dict.get(handful[1])
+
+                if handful[0] > minimum:
+
+                    dict.update({handful[1] : int(handful[0])})
+
+        # Cube the minimums together and add its result to the sum.
+
+        cubed = dict.get('blue') * dict.get('green') * dict.get('red')
+
+        sum += cubed
+
+    return sum
+
+print(cubed(input))
