8e81b78a40cee085f343f9e433bca6b13e9526da
John Gibbs <hello@johnhenrygibbs.com>
2024-01-02 22:05:33 -0800

    Solution for Part One

--- 2023/Day 4/script.py
+++ 2023/Day 4/script.py
@@ -3,3 +3,34 @@ with open('/Users/johngibbs/Desktop/John/Repositories/advent-of-code/2023/Day 4/
 
 # Solution for Part One
 
+def scratchcards(input):
+
+    total = 0
+
+    for line in input:
+
+        points = 0
+
+        card = line.split("|")
+        winningNumbers = card[0][9:].split()
+        myNumbers = card[1].split()
+
+        for i in winningNumbers:
+
+            for j in myNumbers:
+
+                if i == j:
+
+                    if points == 0:
+
+                        points = 1
+
+                    else:
+
+                        points *= 2
+
+        total += points
+
+    return total
+
+print(scratchcards(input))
