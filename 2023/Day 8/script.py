bbc68e12d7b5d32d950625057d0c0220fa13ebdf
John Gibbs <hello@johnhenrygibbs.com>
2024-01-09 21:29:45 -0800

    Solution for Part Two

--- 2023/Day 8/script.py
+++ 2023/Day 8/script.py
@@ -1,3 +1,5 @@
+from math import lcm
+
 with open('/Users/johngibbs/Desktop/John/Repositories/advent-of-code/2023/Day 8/input.txt') as file:
     input = file.read().splitlines()
 
@@ -28,7 +30,7 @@ location = 'AAA'
 
 # The hauntedWasteland() function returns the total steps needed to traverse the desert.
 
-def hauntedWasteland(location, myDictionary):
+def hauntedWasteland(location, dictionary):
 
     totalSteps = 0
 
@@ -40,18 +42,93 @@ def hauntedWasteland(location, myDictionary):
 
             # The paths variable is an array of two options that represent the next key to jump to based on a command.
 
-            paths = myDictionary[location]
+            paths = dictionary[location]
 
             if command == 'L':
 
                 location = paths[0]
-                totalSteps += 1
 
             elif command == 'R':
 
                 location = paths[1]
-                totalSteps += 1
+
+            totalSteps += 1
 
     return totalSteps
 
 print(hauntedWasteland(location, myDictionary))
+
+# Solution for Part Two
+
+# The "ghost" maps for part two are synced so that each path neatly loops from A to Z. Therefore, finding the least common multiple of paths is one solution.
+
+# Start by finding the nodes that end with 'A' in the dictionary. There are 6 in the input data.
+
+def findNodes(dictionary):
+
+    nodes = []
+
+    for key in dictionary:
+
+        if key[2] == 'A':
+
+            nodes.append(key)
+
+    return nodes
+
+nodes = findNodes(myDictionary)
+
+# Create a helper function to determine when a node ends in the letter 'Z'.
+
+def endsWithZed(node):
+
+    theEnd = True
+
+    if node[2] != 'Z':
+
+        theEnd = False
+
+    return theEnd
+
+def ghostMap(nodes, dictionary):
+
+    totalSteps = []
+
+    # Iterate through the nodes list one at a time and determine how many steps it takes to reach the end.
+
+    for node in nodes:
+
+        nodeSteps = 0
+        i = 0
+
+        while endsWithZed(node) == False:
+
+            paths = dictionary[node]
+
+            if instructions[i] == 'L':
+
+                node = paths[0]
+
+            elif instructions[i] == 'R':
+
+                node = paths[1]
+
+            nodeSteps += 1
+
+            # To start over, the instructions index is hard coded to reset once it reaches the end.
+
+            if i == 268:
+
+                i = 0
+
+            else:
+
+                i += 1
+
+        totalSteps.append(nodeSteps)
+
+    return totalSteps
+
+totals = ghostMap(nodes, myDictionary)
+
+print(lcm(*totals))
